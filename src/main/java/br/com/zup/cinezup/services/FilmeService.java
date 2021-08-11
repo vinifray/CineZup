baf44488e6d4ba6e5fe8c3648e03077b1c6afb86
Vinícius <vinicius.raymundo@zup.com.br>
2021-08-11 15:22:15 -0300

    Criar classe de serviço para filme

---
+++ src/main/java/br/com/zup/cinezup/services/FilmeService.java
@@ -0,0 +1,12 @@
+package br.com.zup.cinezup.services;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class FilmeService {
+    private List<FilmeService> filmes = new ArrayList<>();
+
+}
