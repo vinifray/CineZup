bc60d75bee8e2db7927b69008874948832b8cdbf
Vinícius <vinicius.raymundo@zup.com.br>
2021-08-11 14:45:53 -0300

    Mapear recurso /atores

--- src/main/java/br/com/zup/cinezup/controllers/AtorController.java
+++ src/main/java/br/com/zup/cinezup/controllers/AtorController.java
@@ -2,9 +2,11 @@ package br.com.zup.cinezup.controllers;
 
 import br.com.zup.cinezup.services.AtorService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/atores")
 public class AtorController {
     @Autowired
     private AtorService atorService;
