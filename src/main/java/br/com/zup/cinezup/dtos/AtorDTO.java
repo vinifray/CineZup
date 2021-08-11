45aa16ae724b9816daac2db7d48925c5605de93e
Vinícius <vinicius.raymundo@zup.com.br>
2021-08-11 14:09:46 -0300

    Criar classe AtorDTO

---
+++ src/main/java/br/com/zup/cinezup/dtos/AtorDTO.java
@@ -0,0 +1,25 @@
+package br.com.zup.cinezup.dtos;
+
+public class AtorDTO {
+    private String nome;
+    private int idade;
+
+    public AtorDTO() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+}
