dbb5a5c26cae0ffa436caae6e0531bb01c555b6e
Vinícius <vinicius.raymundo@zup.com.br>
2021-08-11 14:13:16 -0300

    Criar classe de service para Ator

---
+++ src/main/java/br/com/zup/cinezup/services/AtorService.java
@@ -0,0 +1,13 @@
+package br.com.zup.cinezup.services;
+
+import br.com.zup.cinezup.dtos.AtorDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class AtorService {
+    private List<AtorDTO> atores = new ArrayList<>();
+
+}
