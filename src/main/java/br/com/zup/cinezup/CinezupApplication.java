28ed019f0abc4e08c2dc842b4a3324813a3bc77f
Vinícius <vinicius.raymundo@zup.com.br>
2021-08-11 14:08:37 -0300

    Iniciar projeto CineZUp

---
+++ src/main/java/br/com/zup/cinezup/CinezupApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.cinezup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinezupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinezupApplication.class, args);
+	}
+
+}
